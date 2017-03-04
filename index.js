15df6da139f486c5a0c5affd8e20d350170edf87
Hobgoblin101 <aj@bilby.net>
2017-03-04 16:07:29 +1100

    Fixed Trace and scan

--- index.js
+++ index.js
@@ -10,26 +10,30 @@ passer.listen(8080);
 passer.get('/data/*', function(req, res){
   let path = req.url.substr(6)+'.dat';
 
-  passer.parseFile('/data/'+path, req, res, false);
+  if (fs.existsSync('./data/'+path)){
+    passer.parseFile('./data/'+path, req, res, false);
+  }else{
+    res.statusCode = 400;
+    res.end('Bad Request\n\n'+'./data/'+path);
+    return;
+  }
 });
 passer.post('/data/*', function(req, res){
-  console.log('form', req.forms);
+  var path = req.url.substr(6);
 
-  let input = req.forms.data;
-
-  try {
-    var data = JSON.parse(input);
-    fs.writeFile(data.line.join('\n'), '/data/'+path+'.dat');
-    res.end('saved');
-  } catch (e) {
+  if (!req.forms.data || !req.forms.data.lines || req.forms.data.lines.length < 1){
+    res.statusCode = 400;
     res.end('Bad Input');
+    return;
   }
+
+  fs.writeFile('./data/'+path+'.dat', req.forms.data.lines.join('\n'), 'utf8', function(){});
+
+  res.end('saved');
 });
 
 passer.get('/wiki/*', function(req, res){
   var path = req.url.substr(6);
-
-  console.log('https://en.wikipedia.org/wiki/'+path);
 
   request('https://en.wikipedia.org/wiki/'+path, function(error, response, body){
     if (error){
